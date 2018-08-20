9d76d4a48717ef080814fbc516255433f87e55d8
ShawnLi1014 <shawnli1014@gmail.com>
2018-08-21 00:04:34 +0800

    update game.js

--- JavaScript/Lecture/Exercise/game.js
+++ JavaScript/Lecture/Exercise/game.js
@@ -2,6 +2,15 @@
 var secretNumber = 4;
 // ask user for guess
 var guess = prompt("Guess a number");
-alert(guess); 
 // check guess
-if
+if(Number(guess) === secretNumber){
+	alert("YOU GOT IT RIGHT!");
+}
+//otherwise, check if higher
+else if(Number(guess) > secretNumber){
+	alert("TOO HIGH, GUESS AGAIN");
+}
+//otherwise, check if lower
+else{
+	alert("TOO LOW, GUESS AGAIN");
+}
